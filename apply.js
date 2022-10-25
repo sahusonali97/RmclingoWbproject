caf45be71a17a8115ad37d3e4fecec5c7c307a9c
sahusonali97 <113846876+sahusonali97@users.noreply.github.com>
2022-10-25 18:37:49 +0530

    Add files via upload

---
+++ apply.js
@@ -0,0 +1,61 @@
+type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDYDbhMChfeHVK8Iojlt0f9H3AtKREyJMc",
+    authDomain: "fire1lingodb.firebaseapp.com",
+    databaseURL: "https://fire1lingodb-default-rtdb.firebaseio.com",
+    projectId: "fire1lingodb",
+    storageBucket: "fire1lingodb.appspot.com",
+    messagingSenderId: "1077059841165",
+    appId: "1:1077059841165:web:e825fbea92c10fdde446c4"
+  };
+
+  
+  // initialize firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // reference your database
+  var contactFormDB = firebase.database().ref("contactForm");
+  
+  document.getElementById("contactForm").addEventListener("submit", submitForm);
+  
+  function submitForm(e) {
+    e.preventDefault();
+  
+    var name = getElementVal("name");
+    var emailid = getElementVal("emailid");
+    var msgContent = getElementVal("msgContent");
+  
+    saveMessages(name, emailid, msgContent);
+  
+    //   enable alert
+    document.querySelector(".alert").style.display = "block";
+  
+    //   remove the alert
+    setTimeout(() => {
+      document.querySelector(".alert").style.display = "none";
+    }, 3000);
+  
+    //   reset the form
+    document.getElementById("contactForm").reset();
+  }
+  
+  const saveMessages = (name, emailid, msgContent) => {
+    var newContactForm = contactFormDB.push();
+  
+    newContactForm.set({
+      name: name,
+      emailid: emailid,
+      msgContent: msgContent,
+    });
+  };
+  
+  const getElementVal = (id) => {
+    return document.getElementById(id).value;
+  };
+  
